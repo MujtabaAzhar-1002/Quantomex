efbcdbc783bececbc369336ca6d8355830824b88
Shaan E Mustafa <143700593+ShaanWork147@users.noreply.github.com>
2023-11-06 20:22:47 +0500

    new

--- routes/clientReviewRoute.js
+++ routes/clientReviewRoute.js
@@ -9,19 +9,76 @@ const { uploader } = require("cloudinary").v2;
 const multer = require("multer");
 const { storage } = require("../cloudinary/index");
 const upload = multer({ storage });
+const Mailjet = require('node-mailjet');
+const mailjet = Mailjet.apiConnect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
 
 // Serve the OTP input form
 router.get("/otpform", (req, res) => {
   res.render("./admin/otpform");
 });
+router.post("/sendOtp", async (req, res) => {
+  // Generate a random 4-digit OTP
+  const otp = Math.floor(1000 + Math.random() * 9000);
+
+  // Get the user's entered email from the request
+  const userEnteredEmail = req.body.email;
+  req.session.otp = otp;
+  // Create an email data object for sending the OTP
+  const emailData = {
+    FromEmail: 'operations@quantomex.com', // Set your email as the sender
+    FromName: 'Quantomex',
+    Recipients: [
+      {
+        Email: userEnteredEmail, // Use the user's entered email address
+        Name: 'User',
+      },
+    ],
+    Subject: 'Your OTP Code',
+    TextPart: `Your OTP code is: ${otp}`,
+    HTMLPart: `
+      <h3>Your OTP Code</h3>
+      <p style="font-size: 16px; color: #333;">Hello,</p>
+      <p style="font-size: 16px; color: #333;">Your OTP code is: ${otp}</p>
+    `,
+  };
+
+  // Send the email with the OTP
+  const otpRequest = mailjet
+    .post('send', { version: 'v3.1' })
+    .request({
+      Messages: [
+        {
+          From: {
+            Email: emailData.FromEmail,
+            Name: emailData.FromName,
+          },
+          To: emailData.Recipients,
+          Subject: emailData.Subject,
+          TextPart: emailData.TextPart,
+          HTMLPart: emailData.HTMLPart,
+        },
+      ],
+    });
+
+  try {
+    await otpRequest;
+    res.redirect('/otpform')
+  } catch (error) {
+    console.error('Error sending OTP email:', error);
+    res.status(500).json({ message: 'Error sending OTP email', error: error.message });
+  }
+});
 
 router.post("/testOtp", (req, res) => {
-  const dummyOTP = "1234";
+  
   const enteredOTP =
     req.body.digit1 + req.body.digit2 + req.body.digit3 + req.body.digit4;
-  if (enteredOTP === dummyOTP) {
+
+  
+ if (enteredOTP === req.session.otp.toString()) {
     return res.redirect("/client-reviews");
   }
+
   res.render("./admin/otpform", {
     error: "Invalid OTP. Please enter the correct OTP.",
   });
@@ -69,7 +126,7 @@ router.post("/deleteClientReview/:id", isAdmin, async (req, res) => {
     res
       .status(500)
       .json({
-        message: "Erro    r deleting Client Review",
+        message: "Error deleting Client Review",
         error: error.message,
       });
   }
--- routes/getQuoteRoute.js
+++ routes/getQuoteRoute.js
@@ -1,36 +1,141 @@
 const express = require('express');
 const router = express.Router();
 const FormEntry = require('../models/FormEntry');
+const Mailjet = require('node-mailjet');
+const mailjet = Mailjet.apiConnect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
 
-router.get('/getquote', (req , res) => {
+router.get('/getquote', (req, res) => {
     res.render('./admin/quotePage');
 });
-router.get('/admin/viewquote', async (req , res) => {
+
+router.get('/admin/viewquote', async (req, res) => {
     getQuoteSubmissions = await FormEntry.find();
-    res.render('./admin/adminQuotePage', { getQuoteSubmissions});
-}); 
+    res.render('./admin/adminQuotePage', { getQuoteSubmissions });
+});
+
 // Handle form submission
 router.post('/submit', async (req, res) => {
   try {
-    const { fullName, email, projectOverview, contactNumber } = req.body;
+      const { fullName, email, projectOverview, contactNumber } = req.body;
 
-    const formEntry = new FormEntry({
-      fullName,
-      email,
-      projectOverview,
-      contactNumber,
-    });
-    await formEntry.save();
-    req.flash('success', 'Quotation Sent Successfully')
-   
-    res.redirect('/getquote'); 
+      const formEntry = new FormEntry({
+          fullName,
+          email,
+          projectOverview,
+          contactNumber,
+      });
+      await formEntry.save();
+
+      // Create the email data object for the user
+      const userEmailData = {
+          FromEmail: 'operations@quantomex.com', // Set your email as the sender
+          FromName: 'Quantomex',
+          Recipients: [
+              {
+                  Email: email, // Use the user's email address from the form
+                  Name: fullName, // Use the user's name
+              },
+          ],
+          Subject: 'Your Quotation Submission',
+          TextPart: 'Thank you for submitting your quotation. We have received your request and will get back to you shortly.',
+          HTMLPart: `
+              <h3>Quotation Submission Confirmation</h3>
+              <p style="font-size: 16px; color: #333;">Hello ${fullName},</p>
+              <p style="font-size: 16px; color: #333;">Thank you for submitting your quotation. We have received your request and will get back to you shortly.</p>
+              <p style="font-size: 16px; color: #333;">Your Details:</p>
+              <ul>
+                  <li style="font-size: 16px; color: #333;">Full Name: ${fullName}</li>
+                  <li style="font-size: 16px; color: #333;">Email: ${email}</li>
+                  <li style="font-size: 16px; color: #333;">Project Overview: ${projectOverview}</li>
+                  <li style="font-size: 16px; color: #333;">Contact Number: ${contactNumber}</li>
+              </ul>
+              <p style="font-size: 16px; color: #333;">We will be in touch with you soon.</p>
+              <p style="font-size: 16px; color: #333;">Thank you for choosing Quantomex.</p>
+          `
+      };
+
+      // Create the email data object for the admin
+      const adminEmailData = {
+          FromEmail: 'operations@quantomex.com', // Set your email as the sender
+          FromName: 'Quantomex',
+          Recipients: [
+              {
+                  Email: 'shaanemustafa8@gmail.com', // Set the admin's email address
+                  Name: 'Admin',
+              },
+          ],
+          Subject: 'New Quote Received',
+          TextPart: `A new quote submission has been received.\n\n
+Full Name: ${fullName}\n
+Email: ${email}\n
+Project Overview: ${projectOverview}\n
+Contact Number: ${contactNumber}`,
+          HTMLPart: `
+              <h3>New Quote Received</h3>
+              <p style="font-size: 16px; color: #333;">Hello Quantomex Team,</p>
+              <p style="font-size: 16px; color: #333;">A new quote submission has been received:</p>
+              <ul>
+                  <li style="font-size: 16px; color: #333;">Full Name: ${fullName}</li>
+                  <li style="font-size: 16px; color: #333;">Email: ${email}</li>
+                  <li style="font-size: 16px; color: #333;">Project Overview: ${projectOverview}</li>
+                  <li style="font-size: 16px; color: #333;">Contact Number: ${contactNumber}</li>
+              </ul>
+              <p style="font-size: 16px; color: #333;">Please review this quote request and take appropriate action.</p>
+              <p style="font-size: 16px; color: #333;">Thank you for your attention.</p>
+          `
+      };
+
+      // Send emails to the user and admin
+      const userRequest = mailjet
+          .post('send', { version: 'v3.1' })
+          .request({
+              Messages: [
+                  {
+                      From: {
+                          Email: userEmailData.FromEmail,
+                          Name: userEmailData.FromName,
+                      },
+                      To: userEmailData.Recipients,
+                      Subject: userEmailData.Subject,
+                      TextPart: userEmailData.TextPart,
+                      HTMLPart: userEmailData.HTMLPart,
+                  },
+              ],
+          });
+
+      const adminRequest = mailjet
+          .post('send', { version: 'v3.1' })
+          .request({
+              Messages: [
+                  {
+                      From: {
+                          Email: adminEmailData.FromEmail,
+                          Name: adminEmailData.FromName,
+                      },
+                      To: adminEmailData.Recipients,
+                      Subject: adminEmailData.Subject,
+                      TextPart: adminEmailData.TextPart,
+                      HTMLPart: adminEmailData.HTMLPart,
+                  },
+              ],
+          });
+
+      // Handle email sending results (for both user and admin emails)
+      const userResponse = await userRequest;
+      const adminResponse = await adminRequest;
+
+     // Redirect to a success page or wherever you want
+     res.redirect('/getquote'); 
+
   } catch (error) {
-    console.error('Error saving form entry:', error);
-    req.flash('error', 'Error sending quotation ');
-    res.status(500).json({ message: 'Error saving form entry', error: error.message });
+      console.error('Error saving form entry:', error);
+      req.flash('error', 'Error sending quotation');
+      res.status(500).json({ message: 'Error saving form entry', error: error.message });
   }
 });
-router.post('/deleteQuote/:id',  async (req, res) => {
+
+
+router.post('/deleteQuote/:id', async (req, res) => {
     try {
         // Find and delete the quote submission by its ID
         const deletedSubmission = await FormEntry.findByIdAndDelete(req.params.id);
@@ -43,4 +148,5 @@ router.post('/deleteQuote/:id',  async (req, res) => {
         res.status(500).json({ message: 'Error deleting quote submission', error: error.message });
     }
 });
+
 module.exports = router;
